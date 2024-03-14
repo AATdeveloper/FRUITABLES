ab353214fe58b815dcbd14324c3b24ff565d447c
ParthNsf <parthsaliya63@gmail.com>
2024-03-14 10:23:17 +0530

    12_03_24_Home_page_create

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import Header from './users/component/header/Header';
+import Home from './users/container/home/Home';
+import Footer from './users/component/footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+
+
+    </>
+
+  );
+}
+
+export default App;
